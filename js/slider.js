acd07e1f9550633a0fdceb9d6181e609712cdc0c
darmia05 <darmia@hawk.iit.edu>
2024-11-13 02:02:22 -0600

    adding javascript file
    this is my slideshow file

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,5 @@
 let slideIndex = 0;
+let isClicking = false;
 
 function showSlides() {
   const slides = document.querySelectorAll('.slide');
@@ -8,15 +9,21 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }
   slides[slideIndex - 1].style.display = 'block';
-  setTimeout(showSlides, 3000);
+  setTimeout(showSlides, 3000); 
 }
 
 function moveSlide(n) {
+  if (isClicking) return; 
+  isClicking = true;
+
   slideIndex += n;
   const slides = document.querySelectorAll('.slide');
   if (slideIndex > slides.length) { slideIndex = 1 }
   if (slideIndex < 1) { slideIndex = slides.length }
+
   showSlides();
+
+  setTimeout(() => { isClicking = false; }, 500);
 }
 
 window.onload = function() {
