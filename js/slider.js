18cc99c4aaa9c71e957114383318e1dd7f7ca54e
darmia05 <darmia@hawk.iit.edu>
2024-11-12 23:29:54 -0600

    adding slider file
    this is my slideshow js file

--- js/slider.js
+++ js/slider.js
@@ -27,4 +27,48 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     animateSlider();
+
+    if (typeof google !== "undefined" && google.maps) {
+        initMap();
+    }
 });
+
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 41.83573, lng: -87.62424 },
+        zoom: 15,
+    });
+
+    const marker = new google.maps.Marker({
+        position: { lat: 41.83573, lng: -87.62424 },
+        map: map,
+        title: "McCormick Student Village",
+    });
+
+    const infoWindow = new google.maps.InfoWindow({
+        content: "<h3>McCormick Student Village</h3><p>The oldest resident building on campus.</p>",
+    });
+
+    marker.addListener("click", () => {
+        infoWindow.open(map, marker);
+    });
+
+    const centerControlDiv = document.createElement("div");
+    const centerControl = createCenterControl(centerControlDiv, map, marker);
+    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
+}
+
+function createCenterControl(controlDiv, map, marker) {
+    const controlUI = document.createElement("div");
+    controlUI.classList.add("custom-control");
+    controlUI.title = "Click to center the map on the marker";
+    controlUI.innerText = "Center Map on Marker";
+    controlDiv.appendChild(controlUI);
+
+    controlUI.addEventListener("click", () => {
+        map.setCenter(marker.getPosition());
+        map.setZoom(15);
+    });
+
+    return controlUI;
+}
