b0a8d0f80ad1f116ab47e63a91673be3631542fd
darmia05 <darmia@hawk.iit.edu>
2024-11-13 03:01:40 -0600

    adding javascript file
    this is my slideshow file

--- js/slider.js
+++ js/slider.js
@@ -5,3 +5,10 @@ function changeSlide(direction) {
     currentIndex = (currentIndex + direction + slides.length) % slides.length;
     document.querySelector('.slider').style.transform = `translateX(-${currentIndex * 100}%)`;
 }
+
+window.onload = () => {
+    const slides = document.querySelectorAll('.slide');
+    if (slides.length > 0) {
+        slides[0].classList.add('active');
+    }
+};
